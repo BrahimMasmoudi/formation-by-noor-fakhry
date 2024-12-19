9d4008e24eeef3ab492380eedce87c6fdc0a938e
BrahimMasmoudi <masmoudi.brahim@gmail.com>
2024-12-19 15:51:43 +0100

    get discover movies and series

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,42 @@
 <template>
+  <div>
+    <section>
+      <h1 class=" mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Featured Movies
+      </h1>
+
+      <ul class="grid grid-cols-5 gap">
+        <li v-for="movie in movies">
+          <MovieCard :movie="movie"/>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h1 class=" mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Featured Tv Series
+      </h1>
+      <ul class="grid grid-cols-5 gap">
+        <li v-for="series in seriesList">
+          <SeriesCard :series="series"/>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
+const movies = useState(() => [])
+const seriesList = useState(() => [])
+
+
+await useFetch('/api/movies/discover', {
+  transform: data => {
+    movies.value = data.movies.results
+    seriesList.value = data.series.results
+  }
+})
+
 
 </script>
 
---
+++ server/api/movies/discover.get.ts
@@ -0,0 +1,44 @@
+import {ca} from "cronstrue/dist/i18n/locales/ca";
+
+export default defineEventHandler(async (event) => {
+    const config = useRuntimeConfig(event)
+    const {accessToken} = config
+
+    console.log(accessToken)
+
+    const movies = await $fetch('https://api.themoviedb.org/3/discover/movie', {
+        method: 'get',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        },
+        query: {
+            include_adult: false,
+            include_video: false,
+            language: 'en-US',
+            page: '1',
+            sort_by: 'popularity.desc'
+        }
+    })
+
+    const series = await $fetch('https://api.themoviedb.org/3/discover/tv', {
+        method: 'get',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+        },
+        query: {
+            include_adult: false,
+            include_null_first_air_dates: false,
+            language: 'en-US',
+            page: '1',
+            sort_by: 'popularity.desc'
+        }
+    })
+
+    return {
+        movies,
+        series
+    }
+
+})
