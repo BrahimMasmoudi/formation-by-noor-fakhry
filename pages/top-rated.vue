fa8a9576b8853538b536438dd0af5cc4628b6cec
BrahimMasmoudi <masmoudi.brahim@gmail.com>
2024-12-20 09:44:46 +0100

    top rated

--- pages/top-rated.vue
+++ pages/top-rated.vue
@@ -1,8 +1,32 @@
 <template>
+  <section>
+    <h1 class=" mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+      Now playing Movies
+    </h1>
+
+    <ul class="grid grid-cols-5 gap">
+      <li v-for="movie in data?.movies?.results ??[]">
+        <MovieCard :movie="movie"/>
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h1 class=" mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+      Now playing TV series
+    </h1>
+
+    <ul class="grid grid-cols-5 gap">
+      <li v-for="serie in data?.series?.results ??[]">
+        <SeriesCard :series="serie"/>
+      </li>
+    </ul>
+  </section>
 
 </template>
 
 <script setup lang="ts">
+const {data} = await useFetch('/api/movies/top-rated')
 
 </script>
 
